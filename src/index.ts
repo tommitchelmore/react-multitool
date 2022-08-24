2a11f42c6213f07c3d3e29c971bfd788665ba015
Thomas Mitchelmore <tommitchelmore@outlook.com>
2022-08-25 00:25:19 +0100

    feat: add all hooks to index export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import useOpen from './hooks/useOpen/useOpen'
-import useLocalData from './hooks/useLocalData/useLocalData'
+import useClickAway from './hooks/useClickAway/useClickAway'
+import useCounter from './hooks/useCounter/useCounter'
+import useEvent from './hooks/useEvent/useEvent'
 import useHover from './hooks/useHover/useHover'
+import useLocalData from './hooks/useLocalData/useLocalData'
+import useMouse from './hooks/useMouse/useMouse'
+import useOpen from './hooks/useOpen/useOpen'
+import usePrevious from './hooks/usePrevious/usePrevious'
+import useQueue from './hooks/useQueue/useQueue'
+import useScrollLock from './hooks/useScrollLock/useScrollLock'
+import useSet from './hooks/useSet/useSet'
 
 export default {
-  useOpen,
+  useClickAway,
+  useCounter,
+  useEvent,
+  useHover,
   useLocalData,
-  useHover
+  useMouse,
+  useOpen,
+  usePrevious,
+  useQueue,
+  useScrollLock,
+  useSet
 }
